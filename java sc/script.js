cb0fcbe25f349e13b2e42db2d875cf7b51461fda
mdshakil129090 <159450943+mdshakil129090@users.noreply.github.com>
2024-07-11 14:07:25 +0600

    Add files via upload

---
+++ java sc/script.js
@@ -0,0 +1,13 @@
+// example 1 alart message
+$(document).ready(function(){
+     alert("welcome to my website");
+
+});
+// example 2 alart message through button
+$(document).ready(function(){
+  $('button').on("click",function(){
+    alert("welcome to my page")
+
+  });
+
+});
